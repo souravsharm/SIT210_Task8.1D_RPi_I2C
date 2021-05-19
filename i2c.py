8d9871b9acb7533e6debc3331f52b279dbe8b816
souravsharm <69393883+souravsharm@users.noreply.github.com>
2021-05-20 03:38:19 +1000

    Update i2c.py

--- i2c.py
+++ i2c.py
@@ -5,18 +5,18 @@ bus = SMBus(1) # indicates /dev/ic2-1
 
 numb = 1
 
-print ("Enter 1 for Blue or 2 for Green or 3 for Red or 0 for OFF")
+print ("On Console enter 1,2,3 for the lights and 0 for OFF ")
 while numb == 1:
 
-	ledstate = input(">>>>   ")
+	ledstate = input(":: ")
 
 	if ledstate == "1":
-		bus.write_byte(addr, 0x1) # switch it on
+		bus.write_byte(addr, 0x1)
 	elif ledstate == "0":
-		bus.write_byte(addr, 0x0) # switch it on
+		bus.write_byte(addr, 0x0)
 	elif ledstate == "2":
-		bus.write_byte(addr, 0x2) # switch it on
+		bus.write_byte(addr, 0x2) 
 	elif ledstate == "3":
-		bus.write_byte(addr, 0x3) # switch it on
+		bus.write_byte(addr, 0x3) 
 	else:
 		numb = 0
