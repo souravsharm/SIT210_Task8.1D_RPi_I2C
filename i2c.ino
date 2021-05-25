3df3a29cb6642117bf92f68fab7e53c4165e33ce
souravsharm <69393883+souravsharm@users.noreply.github.com>
2021-05-26 02:51:07 +1000

    ArduinoCode_i2c_8.1D.io

--- i2c.ino
+++ i2c.ino
@@ -1,12 +1,8 @@
-
-
 // Includin the Wire library for I2C
 #include <Wire.h>
 
 // LED position
-const int red = 11;  
-const int green = 13;  
-const int yellow = 16;   
+const int red = 13;  
 
 void setup() {
   // Join I2C bus as slave with address 8
@@ -18,10 +14,7 @@ void setup() {
   // Setup pin 13 as output and turn LED off
   pinMode(red, OUTPUT);  
   digitalWrite(red, LOW); 
-  pinMode(green, OUTPUT);
-  digitalWrite(green, LOW);
-  pinMode(yellow, OUTPUT);
-  digitalWrite(yellow, LOW);
+
 }
 
 // Function that executes whenever data is received from master
@@ -32,30 +25,16 @@ void receiveEvent(int howMany) {
     if(c == 0)
     {
       digitalWrite(red, 0); 
-      digitalWrite(green, 0); 
-      digitalWrite(yellow, 0); 
+      
     }
     
     else if(c == 1)
     {
       digitalWrite(red, 1); 
-      digitalWrite(green, 0); 
-      digitalWrite(yellow, 0);  
+       
     }
     
-    else if(c == 2)
-    {
-      digitalWrite(red, 0);  
-      digitalWrite(green, 1); 
-      digitalWrite(yellow, 0); 
-    }
-
-    else if(c == 3)
-    {
-      digitalWrite(red, 0);  
-      digitalWrite(green, 0);  
-      digitalWrite(yellow, 1);   
-    }
+   
   }
 }
 
