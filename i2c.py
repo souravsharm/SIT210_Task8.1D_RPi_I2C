e7cabfab659a410c1097275c29e2c8b7db263ed2
souravsharm <69393883+souravsharm@users.noreply.github.com>
2021-05-26 02:49:37 +1000

    RaspberryCode_i2c_8.1D

--- i2c.py
+++ i2c.py
@@ -1,22 +1,18 @@
 from smbus import SMBus
-
+ 
 addr = 0x8 # bus address
 bus = SMBus(1) # indicates /dev/ic2-1
-
-numb = 1
-
-print ("On Console enter 1,2,3 for the lights and 0 for OFF ")
-while numb == 1:
-
-	ledstate = input(":: ")
-
-	if ledstate == "1":
-		bus.write_byte(addr, 0x1)
-	elif ledstate == "0":
-		bus.write_byte(addr, 0x0)
-	elif ledstate == "2":
-		bus.write_byte(addr, 0x2) 
-	elif ledstate == "3":
-		bus.write_byte(addr, 0x3) 
-	else:
-		numb = 0
+ 
+num= 1
+ 
+print ("Enter 1 for ON and 0 for OFF")
+while num== 1:
+ 
+    led= input(">>>>   ")
+ 
+    if led== "1":
+        bus_.write_byte(addr, 0x1) # switch it on
+    elif led== "0":
+        bus_.write_byte(addr, 0x0)
+    else:
+        i= 0
